c38e8a6b148595c4bcc0d8ca8b6e4eb74cc51c9a
Alican ATAMER <atameralican@gmail.com>
2025-10-30 23:55:06 +0300

    rev

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hftwadbzxuaxhjzidagt.supabase.co',
+        pathname: '/storage/v1/object/public/oyun-gorselleri/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
